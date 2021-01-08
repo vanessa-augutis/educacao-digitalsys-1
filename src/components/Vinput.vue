a38083007e19cae4be01e12f91f4d37fe9b7b7b5
Vanessa Augutis <vanessa.augutis@digitalsys.com.br>
2021-01-08 15:56:08 -0300

    compenente input

--- src/components/Vinput.vue
+++ src/components/Vinput.vue
@@ -1,13 +1,32 @@
 <template>
-  
+  <input class="input" :type="type" :placeholder="placeholder"/>
 </template>
 
 <script>
 export default {
+ props: {
+    placeholder: String,
+    type: String,
 
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.input{
+    width:150px;
+    height: 30px;
+    margin: 5px 15px;
+    color:black;
+}
+
+.input::placeholder{
+    color:black;
+}
+
+.input:focus{
+    outline: none;
+}
 
 </style>
+
--- src/views/listaAtividades.vue
+++ src/views/listaAtividades.vue
@@ -5,15 +5,22 @@
       <Botao mensagem="Nova Atividade" />
    </ContainerBotoes>
   </div>
+  <div>
+    <Vinput type="text" placeholder="CPF do paciente"></Vinput>
+    <Vinput type="text" placeholder="Status do Aprazamento"></Vinput>
+    <Vinput type="text" placeholder="Data"></Vinput>
+  </div>
 </template>
 
 <script>
 import Botao from '@/components/Vbotao.vue'
 import ContainerBotoes from '@/components/VcontainerBotoes.vue'
+import Vinput from '../components/Vinput.vue'
 export default {
   components: {
     Botao,
-    ContainerBotoes
+    ContainerBotoes,
+    Vinput,
   }
 
 }
